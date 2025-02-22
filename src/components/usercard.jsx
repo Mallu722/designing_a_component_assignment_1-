a687e0777a2b8988d8359a74d5f12f88f6f82fd1
Mallikarjun722 <anandhiremath979@gmail.com>
2025-02-22 09:44:31 +0530

    added image

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,15 @@
-// write the component code here
+const Usercard = () => {
+    return (
+      <div className="usercard">
+        <img src= "https://thumbs.dreamstime.com/b/profile-picture-caucasian-male-employee-posing-office-happy-young-worker-look-camera-workplace-headshot-portrait-smiling-190186649.jpg" 
+        />
+        <div className="user-details">
+          <h1>John Doe</h1>
+          <h3>Email:john722@gmail.com</h3>
+          <h3>Phone: 1234567890</h3>
+          <h3>Address: 123 Main St, Anytown, USA</h3>
+          </div>
+          </div>
+      );
+  };
+  export default Usercard;
